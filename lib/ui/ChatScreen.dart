ec1f093cbfdea2dc0904cc4f9f07c4344e33190c
Rohith K <37647839+rohithkye@users.noreply.github.com>
2021-02-18 14:24:54 +0530

    18022021
    todo
    {drawer,
    android compute function error,
    settings,
    profile,
    chatscreen}

--- lib/ui/ChatScreen.dart
+++ lib/ui/ChatScreen.dart
@@ -1,8 +1,10 @@
 import 'dart:core';
 import 'dart:ui';
 import 'package:bubble/bubble.dart';
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/model/messageModel.dart';
+import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
 
 // ignore: must_be_immutable
 class ChatScreen extends StatefulWidget {
@@ -11,22 +13,44 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  TextEditingController _textEditingController = TextEditingController();
+  ScrollController _scrollController = ScrollController();
   String name, lastSeen;
-  // List<Message> _messages;
-  Message msg;
-  List<String> messages = [
-    "Hello",
-    "Si",
-    "sfaim",
-    "unfosfnv",
-    "afoisof",
+  bool isLoading = false;
+  final List<Message> messages = [
+    Message(1, "hello", "time", 1),
+    Message(2, "hi", "time", 2),
+    Message(3, "123", "time", 1),
+    Message(4, "5436", "time", 2),
+    Message(5, "sgzd", "time", 1),
+    Message(6, "ygtdj", "time", 1),
+    Message(7, "rhdhr", "time", 1),
+    Message(8, "niiol", "time", 2),
+    Message(9, "lioshfo", "time", 1),
+    Message(10, "apwodk", "time", 1),
+    Message(11, "poszc", "time", 1),
+    Message(1, "hello", "time", 1),
+    Message(2, "hi", "time", 2),
+    Message(3, "123", "time", 1),
+    Message(4, "5436", "time", 2),
+    Message(5, "sgzd", "time", 1),
+    Message(6, "ygtdj", "time", 1),
+    Message(7, "rhdhr", "time", 1),
+    Message(8, "niiol", "time", 2),
+    Message(9, "lioshfo", "time", 1),
+    Message(10, "apwodk", "time", 1),
+    Message(11, "poszc", "time", 1),
   ];
 
   @override
   Widget build(BuildContext context) {
     final Map args = ModalRoute.of(context).settings.arguments;
     return Scaffold(
+      key: _scaffoldKey,
       appBar: _appBar(context, args),
+      endDrawer: buildDrawer(args, context),
       body: Container(
         child: Column(
           children: [
@@ -42,38 +66,88 @@ class _ChatScreenState extends State<ChatScreen> {
                       bottomRight: Radius.circular(25.0)),
                 ),
                 width: (MediaQuery.of(context).size.width),
+                padding: EdgeInsets.symmetric(vertical: 25.0),
                 child: ListView.builder(
+                  reverse: true,
+                  controller: _scrollController,
                   itemCount: messages.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Bubble(
-                      padding: BubbleEdges.all(2),
-                      alignment: Alignment.center,
-                      color: Color.fromRGBO(212, 234, 244, 1.0),
-                      child: Text(messages[index],
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 11.0)),
+                  itemBuilder: (context, index) {
+                    return messages[index].messageType==2?
+                      Bubble(
+                      margin: BubbleEdges.only(top: 10, right: 40),
+                      alignment: Alignment.topLeft,
+                      nip: BubbleNip.leftTop,
+                      child: Text(messages[index].message,style: TextStyle(fontSize: 18),),
+                    ):Bubble(
+                      margin: BubbleEdges.only(top: 10, left: 40),
+                      alignment: Alignment.topRight,
+                      nip: BubbleNip.rightTop,
+                      color: Theme.of(context).backgroundColor,
+                      // color: Colors.blueGrey,
+                      child: Text(messages[index].message,style: TextStyle(fontSize: 18),),
                     );
                   },
                 ),
-                // height: (MediaQuery.of(context).size.height*0.8),
               ),
+              // height: (MediaQuery.of(context).size.height*0.8),
             ),
             bottomNavigationBar(context),
           ],
         ),
       ),
-      // bottomNavigationBar: bottomNavigationBar(context),
+    );
+  }
+
+  Drawer buildDrawer(Map args, BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Container(
+              child: Container(
+                child: Image.network(
+                  args['image'],
+                  fit: BoxFit.cover,
+                  loadingBuilder: (BuildContext context, Widget child,
+                      ImageChunkEvent loadingProgress) {
+                    if (loadingProgress == null) return child;
+                    return Center(
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        value: loadingProgress.expectedTotalBytes != null
+                            ? loadingProgress.cumulativeBytesLoaded /
+                                loadingProgress.expectedTotalBytes
+                            : null,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text('Close Drawer'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+              // Then close the drawer.
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
     );
   }
 
   AppBar _appBar(BuildContext context, Map arg) {
     return AppBar(
+      automaticallyImplyLeading: false,
       elevation: 0.0,
       title: Column(
         children: [
           Text(arg['name']['firstName']),
           Text(
-            arg['isOnline']?"online":arg['lastActive'],
+            arg['isOnline'] ? "online" : arg['lastActive'],
             style: TextStyle(
               fontSize: 10,
             ),
@@ -90,17 +164,18 @@ class _ChatScreenState extends State<ChatScreen> {
             backgroundImage: NetworkImage(arg['image']),
             child: GestureDetector(
               onTap: () {
-                Navigator.pushNamed(context, "accountScreen", arguments: {
-                  'name': {
-                    'firstName': arg['name']['firstName'],
-                    'lastName': arg['name']['lastName'],
-                  },
-                  'image': arg['image'],
-                  'status': arg['status'],
-                  'number': arg['number'],
-                  'lastActive': arg['lastActive'],
-                  'isOnline': arg['isOnline'],
-                });
+                _scaffoldKey.currentState.openEndDrawer();
+                // Navigator.pushNamed(context, "accountScreen", arguments: {
+                //   'name': {
+                //     'firstName': arg['name']['firstName'],
+                //     'lastName': arg['name']['lastName'],
+                //   },
+                //   'image': arg['image'],
+                //   'status': arg['status'],
+                //   'number': arg['number'],
+                //   'lastActive': arg['lastActive'],
+                //   'isOnline': arg['isOnline'],
+                // });
               },
               // child: Icon(Icons.face),
             ),
@@ -157,26 +232,22 @@ class _ChatScreenState extends State<ChatScreen> {
                     shape: CircleBorder(),
                     constraints: BoxConstraints.tight(Size.fromRadius(20)),
                   ),
-                  // child: FloatingActionButton(
-                  //   mini: true,
-                  //   child: Icon(Icons.add),
-                  //   onPressed: (){},
-                  //   // onPressed: modalBottomSheetMenu(),
-                  // ),
                 ),
                 Expanded(
-                    child: TextField(
-                  decoration: new InputDecoration(
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      enabledBorder: InputBorder.none,
-                      errorBorder: InputBorder.none,
-                      disabledBorder: InputBorder.none,
-                      contentPadding: EdgeInsets.only(left: 5, right: 5),
-                      hintText: "Type a message."),
-                )),
+                  child: TextField(
+                    controller: _textEditingController,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                        contentPadding: EdgeInsets.only(left: 5, right: 5),
+                        hintText: "Type a message."),
+                  ),
+                ),
                 RawMaterialButton(
-                  onPressed: () {},
+                  onPressed: _send,
                   // elevation: 2.0,
                   fillColor: Theme.of(context).primaryColor,
                   child: Icon(
@@ -197,5 +268,16 @@ class _ChatScreenState extends State<ChatScreen> {
         ),
       ),
     );
+  }
+
+  _send() {
+    setState(() {
+      messages.insert(
+          0,
+          Message(messages.length + 1, _textEditingController.value.text,
+              "time", 1));
+      _textEditingController.clear();
+    });
+    print(messages.last.message);
   }
 }
