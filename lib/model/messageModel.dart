87d7304ff687dc74cad859dc4fa9fd0182015658
Rohithkye <rohithkye@gmail.com>
2021-03-23 22:55:43 +0530

    gitIgnore

--- lib/model/messageModel.dart
+++ lib/model/messageModel.dart
@@ -1,5 +1,10 @@
 class Message {
   String message,time;
+  int messageType;
   int id;
-  Message(this.id,this.message);
+  Message(this.id,this.message,this.time,this.messageType);
+  
+  factory Message.fromJSON(Map<String,dynamic> json){
+    return Message(json['msgID'], json['data'], json['time'],json['messageType']);
+  }
 }
